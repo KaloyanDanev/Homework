4f781fbd6f6ba87f15dd980f559ee27e8c617fc0
KaloyanDanev <kaloian4obg99@gmail.com>
2017-09-25 17:16:15 +0300

    01.IntroandBasicSyntaxExercises

---
+++ 01.IntroandBasicSyntaxExercises/04.BeverageLabels/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _04.BeverageLabels
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var product = Console.ReadLine();
+            var volume = int.Parse(Console.ReadLine());
+            var energy = int.Parse(Console.ReadLine());
+            var sugar = int.Parse(Console.ReadLine());
+
+            var totalenergy = volume * energy / 100.0;
+            var sugars = sugar * volume / 100.0;
+
+            Console.WriteLine("{0}ml {1}:", volume, product);
+            Console.WriteLine("{0}kcal, {1}g sugars", totalenergy, sugars);
+        }
+    }
+}
---
+++ 01.IntroandBasicSyntaxExercises/Programming Fundamentals/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Programming_Fundamentals
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var prof = Console.ReadLine();
+            var quantity = int.Parse(Console.ReadLine());
+            if (prof == "Athlete")
+            {
+                var total = quantity * 0.70;
+                Console.WriteLine($"The Athlete has to pay {total:f2}.");
+            }
+            else if (prof == "Businessman")
+            {
+                var total = quantity * 1.00;
+                Console.WriteLine($"The Businessman has to pay {total:f2}.");
+            }
+            else if (prof == "Businesswoman")
+            {
+                var total = quantity * 1.00;
+                Console.WriteLine($"The Businesswoman has to pay {total:f2}.");
+            }
+            else if (prof == "SoftUni Student")
+            {
+                var total = quantity * 1.70;
+                Console.WriteLine($"The SoftUni Student has to pay {total:f2}.");
+            }
+            else
+            {
+                var total = quantity * 1.20;
+                Console.WriteLine($"The {prof} has to pay {total:f2}.");
+            }
+        
+        }
+    }
+}
