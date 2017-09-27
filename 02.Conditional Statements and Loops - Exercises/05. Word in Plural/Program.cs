338a4483130b50fd595f158691127ef6d65ceec8
KaloyanDanev <kaloian4obg99@gmail.com>
2017-09-27 17:14:52 +0300

    homework

---
+++ 02.Conditional Statements and Loops - Exercises/05. Word in Plural/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _05.Word_in_Plural
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string word = Console.ReadLine();
+            bool endY = word.EndsWith("y");
+            bool end = word.EndsWith("o") || word.EndsWith("ch") || word.EndsWith("s") ||
+                word.EndsWith("sh") || word.EndsWith("x") || word.EndsWith("z");
+            if (end)
+            {
+                word += "es";
+            }
+            else if (endY)
+            {
+                word = word.Remove(word.Length - 1) + "ies";
+            }
+            else
+            {
+                word += "s";
+            }
+            Console.WriteLine(word);
+        }
+    }
+}
