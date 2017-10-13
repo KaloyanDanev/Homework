467cd6d86be8a13c67842b3322a616dd8a2edebd
KaloyanDanev <kaloian4obg99@gmail.com>
2017-10-13 14:00:03 +0300

    Homework

---
+++ 01.Intro and Basic Syntax - Exercises/04. BeverageLabels/CakeIngridients/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CakeIngridients
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var ingredient = Console.ReadLine();
+            var ingedintsCnt = 0;
+
+            while (ingredient != "Bake!")
+            {
+                ingedintsCnt++;
+                Console.WriteLine($"Adding ingredient {ingredient}.");
+                ingredient = Console.ReadLine();
+            }
+            Console.WriteLine($"Preparing cake with {ingedintsCnt} ingredients.");
+        }
+    }
+}
