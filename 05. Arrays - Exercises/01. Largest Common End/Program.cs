68c1d3d6b1074575dc5bbd05c74929f7abaebfc8
KaloyanDanev <kaloian4obg99@gmail.com>
2017-10-13 13:57:47 +0300

    Homework

---
+++ 05. Arrays - Exercises/01. Largest Common End/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Arrays___Exercises
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] firstArr = Console.ReadLine()
+                .Split(new char[] { ' ' },StringSplitOptions.RemoveEmptyEntries);
+
+            string[] secondArr = Console.ReadLine()
+                 .Split(new char[] { ' ' },StringSplitOptions.RemoveEmptyEntries);
+
+            int leftCount = FindMaxCommonItems(firstArr, secondArr);
+            Array.Reverse(firstArr);
+            Array.Reverse(secondArr);
+            int rightCount = FindMaxCommonItems(firstArr, secondArr);
+        
+            Console.WriteLine($"{Math.Max(leftCount,rightCount)}");
+        }
+            static int FindMaxCommonItems(string[] firstArr, string[] secondArr)
+            {
+            int length = Math.Min(firstArr.Length, secondArr.Length);
+            int count = 0;
+            for (int i = 0; i < length; i++)
+            {
+                if (firstArr[i] == secondArr[i])
+                {
+                    count++;
+                }
+                else
+                {
+                    break;
+                }
+            }
+            return count;
+        }
+    }
+}
---
+++ 05. Arrays - Exercises/02. Rotate and Sum/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02.Rotate_and_Sum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] number = Console.ReadLine()
+               .Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries)
+               .Select(int.Parse)
+               .ToArray();
+            int rotations = int.Parse(Console.ReadLine());
+
+            int[] sum = new int[number.Length];
+
+            for (int j = 0; j < rotations; j++)
+            {
+
+                int reminder = number[number.Length - 1];
+                for (int i = number.Length - 1; i > 0; i--)
+                {
+                    number[i] = number[i - 1];
+                    sum[i] += number[i];
+                }
+                number[0] = reminder;
+                sum[0] += number[0];
+            }
+            Console.WriteLine($"{string.Join(" ",sum)}");
+        }
+    }
+}
